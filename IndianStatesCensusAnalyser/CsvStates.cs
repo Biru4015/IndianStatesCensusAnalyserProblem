dca2fe53439891407ef0a433a7a4af6eb4f3cbec
Biru4015 <rambirendra12@gmail.com>
2020-05-01 21:52:31 +0530

    [Birendra]Add added. check the number of record matches

---
+++ IndianStatesCensusAnalyser/CsvStates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyser
+{
+    /// <summary>
+    /// Thhis class contains the code for load the CSV data 
+    /// </summary>
+    class CsvStates
+    {
+        /// <summary>
+        /// This method is created for finding number of records
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public static int getnumberOfRecords(string filepath)
+        {
+            int count = 0;
+            string[] elements = File.ReadAllLines(filepath);
+            IEnumerable<string> e = elements;
+            foreach (var element in e)
+            {
+                count++;
+            }
+            return count - 1;
+        }
+    }
+}
---
+++ IndianStatesCensusAnalyser/StateCensusAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace IndianStatesCensusAnalyser
+{
+    /// <summary>
+    /// This class contains the main method main of Indian states census analyser problem
+    /// </summary>
+    public class StateCensusAnalyser
+    {
+        private object filepath;
+
+        /// <summary>
+        /// This is main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to India state census Analyzer");
+        }
+        public static int numberOfRecords(string filepath)
+        {
+            string[] data = File.ReadAllLines(filepath);
+            return data.Length - 1;
+        }
+    }
+}

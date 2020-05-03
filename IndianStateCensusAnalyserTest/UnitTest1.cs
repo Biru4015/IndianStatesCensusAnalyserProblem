93b205732584e6bd85ff2a7c2ff89d4d587efbd7
Biru4015 <rambirendra12@gmail.com>
2020-05-03 10:27:36 +0530

    [Birendra]Refactor. refactored. doing some changes in code

--- IndianStateCensusAnalyserTest/UnitTest1.cs
+++ IndianStateCensusAnalyserTest/UnitTest1.cs
@@ -1,70 +1,75 @@
-using IndianStatesCensusAnalyser;
-using NUnit.Framework;
 namespace IndianStateCensusAnalyserTest
 {
+    using IndianStatesCensusAnalyser;
+    using NUnit.Framework;
+
     /// <summary>
     /// This class contains the code for testing all the use cases
     /// </summary>
     public class Tests
     {
-        public string filepath = @"C:\Users\Birendra Kumar\source\repos\CensusData\StateCensusData.csv";
+        /// <summary>
+        /// Reading the file path
+        /// </summary>
+        private string filePath = @"C:\Users\Birendra Kumar\source\repos\IndianStatesCensusAnalyser\Files\StateCensusData.csv";
+
         /// <summary>
         /// Test case 1.1
         /// For checking number of records in StateCensusData are matches or not
         /// </summary>
         [Test]
-        public void givenNumOfState_whaeCheckingnumberOfRecords_shouldReturnsNumOfRecords()
+        public void GivenNumOfState_WhenCheckingnumberOfRecords_ShouldReturnsNumOfRecords()
         {
-            int numberofRecords = (int)StateCensusAnalyser.records(filepath);
+            int numberofRecords = (int)StateCensusAnalyser.Records(this.filePath);
             Assert.AreEqual(29, numberofRecords);
         }
 
         /// <summary>
         /// Test case 1.2
-        /// For checking when incorrect file name enterd raised exception
+        /// For checking when incorrect file name entered raised exception
         /// </summary>
         [Test]
-        public void givenFileNameIncorect_whenCheckingFilePath_shouldReturnsFileNotFoundeException()
+        public void WivenFileNameIncorect_WhenCheckingFilePath_ShouldReturnsFileNotFoundeException()
         {
-            string actualPath = @"C:\Users\Birendra Kumar\source\repos\CensusData\StateCensusDatas.csv";
+            string actualPath = @"C:\Users\Birendra Kumar\source\repos\IndianStatesCensusAnalyser\Files\StateCensusDatas.csv";
             StateCensusAnalyser stateCensus = new StateCensusAnalyser(actualPath);
-            Assert.AreEqual("File not found", stateCensus.numberOfRecords());
+            Assert.AreEqual("File not found", stateCensus.NumberOfRecords());
         }
 
         /// <summary>
-        ///Test case 1.3
-        ///When csv file is correct but type is incorrect
+        /// Test case 1.3
+        /// When comma separated value file is correct but type is incorrect
         /// </summary>
         [Test]
-        public void givenIncorrectFileFormat_whenCheckingFileFormat_shouldReturnsCustomException()
+        public void GivenIncorrectFileFormat_WhenCheckingFileFormat_ShouldReturnsCustomException()
         {
-            string actualPath = @"C:\Users\Birendra Kumar\source\repos\CensusData\StateCensusData.txt";
+            string actualPath = @"C:\Users\Birendra Kumar\source\repos\IndianStatesCensusAnalyser\Files\StateCensusData.txt";
             StateCensusAnalyser stateCensus = new StateCensusAnalyser(actualPath);
-            Assert.AreEqual("File format Incorrect", stateCensus.numberOfRecords());
+            Assert.AreEqual("File format Incorrect", stateCensus.NumberOfRecords());
         }
 
         /// <summary>
-        ///Test case 1.4
-        ///csv file Correct but delimiter Incorrect
+        /// Test case 1.4
+        /// comma separated value file Correct but delimiter Incorrect
         /// </summary>
         [Test]
-        public void whenDelimeterNotPresent_whenCkeckingInFile_shouldReturnsCustomException()
+        public void WhenDelimeterNotPresent_WhenCkeckingInFile_ShouldReturnsCustomException()
         {
-            string actualPath = @"C:\Users\Birendra Kumar\source\repos\CensusData\StateCensusData.csv";
+            string actualPath = @"C:\Users\Birendra Kumar\source\repos\IndianStatesCensusAnalyser\Files\StateCensusData.csv";
             StateCensusAnalyser stateCensus = new StateCensusAnalyser(actualPath);
-            Assert.AreEqual("Delimiter Incorrect", stateCensus.numberOfRecords());
+            Assert.AreEqual("Delimiter Incorrect", stateCensus.NumberOfRecords());
         }
 
         /// <summary>
-        ///Test case 1.5
-        ///csv file Correct but header name is incorrect in file
+        /// Test case 1.5
+        /// comma separated value file Correct but header name is incorrect in file
         /// </summary>
         [Test]
-        public void whenHeaderIncorrect_whenAnalyse_shouldReturnCustomException()
+        public void WhenHeaderIncorrect_WhenAnalyse_ShouldReturnCustomException()
         {
-            string actualPath = @"C:\Users\Birendra Kumar\source\repos\CensusData\HeaderWrong\StateCensusData.csv";
+            string actualPath = @"C:\Users\Birendra Kumar\source\repos\IndianStatesCensusAnalyser\Files\StateCensusData_Header.csv";
             StateCensusAnalyser stateCensus = new StateCensusAnalyser(actualPath);
-            Assert.AreEqual("Header Incorrect", stateCensus.headerIncorrect());
+            Assert.AreEqual("Header Incorrect", stateCensus.HeaderIncorrect());
         }
     }
 }
